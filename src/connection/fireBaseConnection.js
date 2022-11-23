213202690be404a8bd1f9feb1347497b17aad668
darioo96 <dariolean10@gmail.com>
2022-11-23 01:07:45 -0300

    Create fireBaseConnection.js

---
+++ src/connection/fireBaseConnection.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHclYOvdqIKBFuEYF6dRUaQ_gNqDGRGpE",
+  authDomain: "chat-test-3d724.firebaseapp.com",
+  projectId: "chat-test-3d724",
+  storageBucket: "chat-test-3d724.appspot.com",
+  messagingSenderId: "716290122428",
+  appId: "1:716290122428:web:e2f7476aabf3af0400e6b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
