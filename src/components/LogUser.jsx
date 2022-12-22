4738caad89386d1002b681b084a4228b0aa6dc2b
darioo96 <dariolean10@gmail.com>
2022-12-22 00:19:31 -0300

    Update LogUser.jsx

--- src/components/LogUser.jsx
+++ src/components/LogUser.jsx
@@ -14,40 +14,24 @@ function LogUser({ setUser }) {
 
   const handleChange = (event) => setCustomUser(event.target.value);
   const handleSubmit = () => setUser(customUser);
-    return (
-      <Box
-        m="auto"
-        display="flex"
-        gap="10"
-        flexDir="column"
-        alignItems="center"
-      >
-        <Avatar bg="blue.300" color="white" size="xl" name={customUser} />
-        <FormControl
-          display="flex"
-          flexDir="column"
-          alignItems="center"
-          gap="2"
+  return (
+    <Box m="auto" display="flex" gap="10" flexDir="column" alignItems="center">
+      <Avatar bg="blue.300" color="white" size="xl" name={customUser} />
+      <FormControl display="flex" flexDir="column" alignItems="center" gap="2">
+        <FormLabel>Nombre Usuario</FormLabel>
+        <Input type="text" textAlign="center" w="l" onChange={handleChange} />
+        <Button
+          rightIcon={<AiOutlineSend />}
+          colorScheme="blue"
+          size="lg"
+          variant="outline"
+          onClick={handleSubmit}
         >
-          <FormLabel>Nombre Usuario</FormLabel>
-          <Input
-            type="text"
-            textAlign="center"
-            w="xl"
-            onChange={handleChange}
-          />
-          <Button
-            rightIcon={<AiOutlineSend />}
-            colorScheme="blue"
-            size="lg"
-            variant="outline"
-            onClick={handleSubmit}
-          >
-            Submit
-          </Button>
-        </FormControl>
-      </Box>
-    );
-  }
+          Submit
+        </Button>
+      </FormControl>
+    </Box>
+  );
+}
 
 export default LogUser;
