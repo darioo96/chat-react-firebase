89e8637de71819b6f05df596a98942d85bbdb1c2
darioo96 <dariolean10@gmail.com>
2022-12-22 23:12:41 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { Flex, Text, Spacer, Avatar } from '@chakra-ui/react'
+import React from "react";
+import { Flex, Text, Spacer, Avatar } from "@chakra-ui/react";
+import User from "./User";
 
-
-function Navbar({user}) {
+function Navbar({ user }) {
   return (
-    <Flex bg='blackAlpha.800' w='30vw' h='100%' p='5' alignItems='start'>
-        <Text color='whiteAlpha.800' as='b'> React - Chat</Text>
-        <Spacer/>
-        <Avatar name={user}  visibility={user? 'visible' : 'hidden'}/>
-    </Flex>
-  )
+    <>
+      <Flex bg="blackAlpha.800" w="30vw" h="100vh" p="5" direction='column'>
+        <Flex>
+          <Text color="whiteAlpha.800" as="b">React - Chat</Text>
+          <Spacer />
+          <Avatar name={user} visibility={user ? "visible" : "hidden"} />
+        </Flex>
+        <User />
+      </Flex>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
