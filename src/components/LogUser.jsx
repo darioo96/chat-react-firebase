3f0675c584ca567578105a2a27f3fa0ced98bf7c
darioo96 <dariolean10@gmail.com>
2022-12-27 00:32:30 -0300

    Update LogUser.jsx

--- src/components/LogUser.jsx
+++ src/components/LogUser.jsx
@@ -6,8 +6,11 @@ import {
   Input,
   Avatar,
   Button,
+  Flex,
 } from "@chakra-ui/react";
 import { AiOutlineSend } from "react-icons/ai";
+import { FaSignInAlt } from "react-icons/fa";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 function LogUser({ setUser }) {
   const [customUser, setCustomUser] = useState("");
@@ -15,20 +18,18 @@ function LogUser({ setUser }) {
   const handleChange = (event) => setCustomUser(event.target.value);
   const handleSubmit = () => setUser(customUser);
   return (
-    <Box m="auto" display="flex" gap="10" flexDir="column" alignItems="center">
+    <Box m="auto" display="flex" gap="3" flexDir="column" alignItems="center">
       <Avatar bg="blue.300" color="white" size="xl" name={customUser} />
-      <FormControl display="flex" flexDir="column" alignItems="center" gap="2">
-        <FormLabel>Nombre Usuario</FormLabel>
-        <Input type="text" textAlign="center" w="l" onChange={handleChange} />
-        <Button
-          rightIcon={<AiOutlineSend />}
-          colorScheme="blue"
-          size="lg"
-          variant="outline"
-          onClick={handleSubmit}
-        >
-          Submit
-        </Button>
+      <FormControl display="flex" flexDir="column" alignItems="center" gap="5">
+        <FormLabel>Iniciar Sesión</FormLabel>
+        <Input type="text" textAlign="center" w="l" onChange={handleChange} placeholder='User'/>
+        <Input type="password" textAlign="center" w="l" placeholder='Password'/>
+        <Flex align='center' gap='2'>
+          <Button size='md' colorScheme="teal" variant="outline" ><FaSignInAlt /></Button>
+          <Button rightIcon={<AiOutlineSend />} colorScheme="blue" size="md" variant="outline" onClick={handleSubmit}>
+           Ingresar
+          </Button>
+        </Flex>
       </FormControl>
     </Box>
   );
