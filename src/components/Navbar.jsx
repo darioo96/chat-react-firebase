aa09bc0de5e4b2cc3f49adc9463deb11b6537cd7
darioo96 <dariolean10@gmail.com>
2022-12-28 14:18:32 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import User from "./User";
 function Navbar({ user }) {
   return (
     <>
-      <Flex bg="blackAlpha.800" w="30vw" h="100vh" p="5" direction='column'>
+      <Flex bg="blackAlpha.700" w="30vw" h="100vh" p="5" direction='column' justify='space-between' borderRightRadius='3xl'>
         <Flex>
           <Text color="whiteAlpha.800" as="b">React - Chat</Text>
           <Spacer />
