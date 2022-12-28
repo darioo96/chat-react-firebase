f7c6db89c58161ba3f2bb4df0c3b16430d0d11d1
darioo96 <dariolean10@gmail.com>
2022-12-28 14:18:30 -0300

    Update LogUser.jsx

--- src/components/LogUser.jsx
+++ src/components/LogUser.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import {
-  Box,
   FormControl,
   FormLabel,
   Input,
   Avatar,
   Button,
   Flex,
+  Card,
+  CardBody,
+  Divider,
+  CardFooter,
 } from "@chakra-ui/react";
 import { AiOutlineSend } from "react-icons/ai";
-import { FaSignInAlt } from "react-icons/fa";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+//import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import SignUser from "./SignUser";
 
 function LogUser({ setUser }) {
   const [customUser, setCustomUser] = useState("");
@@ -18,20 +21,26 @@ function LogUser({ setUser }) {
   const handleChange = (event) => setCustomUser(event.target.value);
   const handleSubmit = () => setUser(customUser);
   return (
-    <Box m="auto" display="flex" gap="3" flexDir="column" alignItems="center">
-      <Avatar bg="blue.300" color="white" size="xl" name={customUser} />
-      <FormControl display="flex" flexDir="column" alignItems="center" gap="5">
-        <FormLabel>Iniciar Sesión</FormLabel>
-        <Input type="text" textAlign="center" w="l" onChange={handleChange} placeholder='User'/>
-        <Input type="password" textAlign="center" w="l" placeholder='Password'/>
-        <Flex align='center' gap='2'>
-          <Button size='md' colorScheme="teal" variant="outline" ><FaSignInAlt /></Button>
-          <Button rightIcon={<AiOutlineSend />} colorScheme="blue" size="md" variant="outline" onClick={handleSubmit}>
-           Ingresar
-          </Button>
-        </Flex>
-      </FormControl>
-    </Box>
+    <Card m='auto' variant='filled' bg='blackAlpha.300'>
+      <CardBody  display="flex" gap="3" flexDir="column" alignItems="center">
+        <Avatar bg="blue.300" color="white" size="xl" name={customUser} />
+        <FormControl display="flex" flexDir="column" alignItems="center" gap="5">
+          <FormLabel>Iniciar Sesión</FormLabel>
+          <Input type="text" textAlign="center" w="l" onChange={handleChange} placeholder='User'/>
+          <Input type="password" textAlign="center" w="l" placeholder='Password'/>
+          <Flex align='center' gap='2'>
+
+            <Button rightIcon={<AiOutlineSend />} colorScheme="blue" size="md" variant="outline" onClick={handleSubmit}>
+            Ingresar
+            </Button>
+          </Flex>
+        </FormControl>
+      </CardBody>
+      <Divider/>
+      <CardFooter justify='center'>
+        <SignUser/>
+      </CardFooter>
+    </Card>
   );
 }
 
