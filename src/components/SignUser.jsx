1d5746c0d8f04e709759e1359753539ab8c60aaa
darioo96 <dariolean10@gmail.com>
2022-12-30 23:37:54 -0300

    Update SignUser.jsx

--- src/components/SignUser.jsx
+++ src/components/SignUser.jsx
@@ -11,18 +11,31 @@ import {
   Input,
   InputRightElement,
   InputGroup,
-  ModalFooter
+  useToast
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-
-
-
+import signAuth from "../connection/signAuth";
 
 function SignUser() {
+    
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [loading, setLoading] = useState(false)
     const [show, setShow] = useState(false)
+    const [newUser, setNewUser] = useState('')
+    const [newMail, setNewMail] = useState('')
+    const [password, setPassword] = useState('')
+    const toast = useToast()
+
+    const handleChangeUser = (event) => setNewUser(event.target.value)
+    const handleChangeMail = (event) => setNewMail(event.target.value)
+    const handleChangePassword = (event) => setPassword(event.target.value)
     const handleClick = () => setShow(!show)
+    const handleSubmit = () => {
+        setLoading(true)
+        signAuth(newUser, newMail, password, onClose,setLoading,toast);
+    }
+
     return(
     <Box>
         <Button onClick={onOpen} rightIcon={<FaSignInAlt/>}>Registrarse</Button>
@@ -31,22 +44,19 @@ function SignUser() {
         <ModalContent>
             <ModalHeader>Registro</ModalHeader>
             <ModalCloseButton />
-            <ModalBody gap='5' display='flex' flexDir='column'>
-                <Input type='email' placeholder="E-mail"/>
+            <ModalBody paddingBottom='5' gap='5' display='flex' flexDir='column'>
+                <Input type='text' placeholder="Nombre de Usuario" onChange={handleChangeUser}/>            
+                <Input type='text' placeholder="E-mail" onChange={handleChangeMail}/>
                 <InputGroup>
-                    <Input type={show ? 'text' : 'password'} placeholder='Contraseña'/>
+                    <Input type={show ? 'text' : 'password'} placeholder='Contraseña' onChange={handleChangePassword}/>
                     <InputRightElement width='4.5rem'>
                         <Button size='sm' onClick={handleClick}>
                         {show ? 'Hide' : 'Show'}
                         </Button>
                     </InputRightElement>
                 </InputGroup>
-
-                
+                <Button colorScheme='green' onClick={handleSubmit} isLoading={loading} >Enviar</Button>                
             </ModalBody>
-            <ModalFooter>
-                <Button colorScheme='green'>Enviar</Button>
-            </ModalFooter>
         </ModalContent>
         </Modal>
     </Box>
