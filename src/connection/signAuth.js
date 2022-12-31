0790aaef5bc61b354e7493aba0d5795027a9448d
darioo96 <dariolean10@gmail.com>
2022-12-30 23:38:02 -0300

    Create signAuth.js

---
+++ src/connection/signAuth.js
@@ -0,0 +1,37 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ref, set } from "firebase/database";
+import { auth, database } from "./fireBaseConnection";
+
+
+export default async function signAuth(user,email,password,onClose,setLoading,toast){
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        writeToDataBaseUser(user,userCredentials.user.uid)
+        setLoading(false)
+        toast({
+            title: `Bienvenido ${user}`,
+            description: "Se creo la cuenta correctamente",
+            status: 'success',
+            duration: 4000,
+            isClosable: true,
+          })
+        onClose()
+
+    } catch (error) {
+        setLoading(false)
+        toast({
+            title: 'Error',
+            description: `${error.code}`,
+            status: 'error',
+            duration: 4000,
+            isClosable: true,
+          })
+    }
+}
+
+const writeToDataBaseUser = (user,uid) => {
+    set(ref(database, `/users/${uid}`), {
+      user
+    });
+  };
+
