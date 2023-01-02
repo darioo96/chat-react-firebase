ed323adcb460377a98f7ab9648962924e42a762b
darioo96 <dariolean10@gmail.com>
2023-01-01 23:14:46 -0300

    Update signIn.js

--- src/connection/signIn.js
+++ src/connection/signIn.js
@@ -1,19 +1,25 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
+import readUser from "./dataUser";
 import { auth } from "./fireBaseConnection";
 
-export async function signIn(email, password, setUser, toast){
-    try {
-        const credentials = await signInWithEmailAndPassword(auth, email, password)
-        setUser(credentials.user);
-    } catch (error) {
-        console.log(error)
-        toast({
-            title: `ERROR`,
-            description: `${error.code}`,
-            status: 'error',
-            duration: 4000,
-            isClosable: true,
-        })
-    }
-    
+export async function signIn(email, password, setUser, toast) {
+  try {
+    const credentials = await signInWithEmailAndPassword(auth, email, password);
+    toast({
+        title: `BIENVENIDO`,
+        status: 'success',
+        duration: 4000,
+        isClosable: true,
+      })
+    readUser(setUser, credentials.user.uid);
+  } catch (error) {
+    console.log(error);
+    toast({
+      title: `ERROR`,
+      description: `${error.code}`,
+      status: "error",
+      duration: 4000,
+      isClosable: true,
+    });
+  }
 }
