74af86c79eb79ba415431c36fd1f519a5a647869
darioo96 <dariolean10@gmail.com>
2023-01-04 17:49:03 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Flex, Text, Spacer, Avatar, Menu, MenuButton, MenuItem, MenuList, Button } from "@chakra-ui/react";
 import User from "./User";
 
-function Navbar({ user }) {
+function Navbar({ user , setUser}) {
+  const  handleClick = () => setUser('')
   return (
     <>
       <Flex bg="blackAlpha.700" w="30vw" h="100vh" p="5" direction='column' justify='space-between' borderRightRadius='3xl'>
@@ -10,11 +11,13 @@ function Navbar({ user }) {
           <Text color="whiteAlpha.800" as="b">React - Chat</Text>
           <Spacer />
           <Menu isLazy>
-            <MenuButton>
-              <Avatar name={user} visibility={user ? "visible" : "hidden"} />
+            <MenuButton visibility={user ? "visible" : "hidden"}>
+              <Avatar name={user}/>
             </MenuButton>
             <MenuList p='0'>
-              <Button  w='100%' variant='ghost'>Cerrar Sesion</Button>
+              <MenuItem>
+              <Button w='100%' variant='ghost' onClick={handleClick}>Cerrar Sesion</Button>
+              </MenuItem>
             </MenuList>
           </Menu>
         </Flex>
