7c44cd1957703fc5686f76e530137d21fabbe8ca
darioo96 <dariolean10@gmail.com>
2023-01-04 17:49:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <ChakraProvider>
       <Flex direction='row' alignItems='center' w='100vw' h='100vh' bgColor='green.50' overflow='hidden'>
-        <Navbar user = {user}/>
+        <Navbar user = {user} setUser={setUser}/>
         <IsLoged user={user} setUser={setUser}/>
       </Flex>
     </ChakraProvider>
