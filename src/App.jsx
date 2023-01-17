b40625ef890315da7013cbf911749b5b86ac08e1
darioo96 <dariolean10@gmail.com>
2023-01-17 00:02:26 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar'
-import { ChakraProvider ,Flex } from '@chakra-ui/react'
+import { ChakraProvider ,extendTheme,Flex } from '@chakra-ui/react'
 import { useState } from 'react'
 
 import IsLoged from './components/IsLoged'
@@ -7,15 +7,29 @@ import IsLoged from './components/IsLoged'
 function App() {
 
   const [user, setUser] = useState("")
+  const theme = extendTheme({
+    colors: {
+      brand: {
+        100: "#BCDD97",
+      },
+    },
+  });
 
   return (
-    <ChakraProvider>
-      <Flex direction='row' alignItems='center' w='100vw' h='100vh' bgColor='green.50' overflow='hidden'>
-        <Navbar user = {user} setUser={setUser}/>
-        <IsLoged user={user} setUser={setUser}/>
+    <ChakraProvider theme={theme}>
+      <Flex
+        direction="row"
+        alignItems="center"
+        w="100vw"
+        h="100vh"
+        bgColor="brand.100"
+        overflow="hidden"
+      >
+        <Navbar user={user} setUser={setUser} />
+        <IsLoged user={user} setUser={setUser} />
       </Flex>
     </ChakraProvider>
-  )
+  );
 }
 
 export default App
