43c66416e30312c9a1a3cd5433c48aeb4ec7db55
darioo96 <dariolean10@gmail.com>
2023-01-27 23:19:48 -0300

    Update fireBaseConnection.js

--- src/connection/fireBaseConnection.js
+++ src/connection/fireBaseConnection.js
@@ -1,14 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBHclYOvdqIKBFuEYF6dRUaQ_gNqDGRGpE",
   authDomain: "chat-test-3d724.firebaseapp.com",
