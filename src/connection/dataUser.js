2e04a50d1103d2d7c7a8a2d18add03838c6b4cdc
darioo96 <dariolean10@gmail.com>
2023-01-27 23:19:45 -0300

    Update dataUser.js

--- src/connection/dataUser.js
+++ src/connection/dataUser.js
@@ -1,8 +1,7 @@
-
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, query, where } from "firebase/firestore";
 import { db } from "../connection/fireBaseConnection";
 
-export default async function readUser(setUser, uid) {
+export default async function readUser(setUser, uid){
   const docUser = await getDoc(doc(db,"users", uid))
   const user = (docUser._document.data.value.mapValue.fields.name.stringValue);
   setUser(user);
